5ac8c904bc65ad6b14e9a5e60f189a140a6f9aa5
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-05-28 18:50:34 +0200

    modified registration form

--- frontend/src/components/RegistrationForm.jsx
+++ frontend/src/components/RegistrationForm.jsx
@@ -1,9 +1,29 @@
+import FormHeader from "./FormComponents/FormHeader";
+import FormGroup from "./FormComponents/FormGroup";
+import FormFooter from "./FormComponents/FormFooter";
+import FormContainer from "./FormComponents/FormContainer";
+import FormButton from "./FormComponents/FormButton";
+
 export default function RegistrationForm(){
     return (
-        <div>
-            <h4>
-                Registration
-            </h4>
-        </div>
+        <FormContainer>
+            <FormHeader headerValue={"Register"} />
+            <form className="p-3">
+                <FormGroup 
+                        labelValue={"Username"}
+                        inputType={"text"}
+                        inputName={"username"}
+                        inputPlaceHolder={"Type your username"}        
+                />
+                <FormGroup 
+                        labelValue={"Password"}
+                        inputType={"password"}
+                        inputName={"password"}
+                        inputPlaceHolder={"Type your password"}
+                />
+                <FormButton buttonText={"Register"} />
+                <FormFooter linkPath={"/"} linkText={"Login"} footerText={"Do you have an account ?"} />
+            </form>
+        </FormContainer>
     );
 };
