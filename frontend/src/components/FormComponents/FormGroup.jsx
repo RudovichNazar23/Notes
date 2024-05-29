dbde1ce6041e9c268f13de271f6f201fa7e59c5d
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-05-29 19:01:09 +0200

    added onSubmit handler for registration form

--- frontend/src/components/FormComponents/FormGroup.jsx
+++ frontend/src/components/FormComponents/FormGroup.jsx
@@ -1,4 +1,4 @@
-export default function FormGroup({ labelValue, inputType, inputPlaceHolder, inputName }){
+export default function FormGroup({ labelValue, inputType, inputPlaceHolder, inputId }){
     return (
         <div className="form-group m-1">
             <label>
@@ -6,7 +6,7 @@ export default function FormGroup({ labelValue, inputType, inputPlaceHolder, inp
                     {labelValue}
                 </strong>
             </label>
-            <input type={inputType} placeholder={inputPlaceHolder} name={inputName} className="form-control" />
+            <input type={inputType} placeholder={inputPlaceHolder} id={inputId} className="form-control" />
         </div>
     );
 };
