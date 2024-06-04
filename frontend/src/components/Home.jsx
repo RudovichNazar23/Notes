023ed621e8824a2d8dd9f57612c9063ea7370ff8
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-06-04 12:28:36 +0200

    modified home page component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,17 @@
+import NavBarButton from "./NavPanelComponents/NavBarButton";
+
 export default function Home(){
     return (
         <div>
-            <h4>
-                Home
-            </h4>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid" >
+                    <NavBarButton buttonValue={"Logout"} />
+                    <NavBarButton buttonValue={"Create new note"} />
+                    <div className="p-1" style={{fontSize: "24px"}}>
+                        User: <string>Nazar</string>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
---
+++ frontend/src/components/NavPanelComponents/NavBarButton.jsx
@@ -0,0 +1,7 @@
+export default function NavBarButton({ buttonValue }){
+    return (
+        <button className="btn" style={{fontSize: "24px"}}>
+            {buttonValue}
+        </button>
+    )
+};
