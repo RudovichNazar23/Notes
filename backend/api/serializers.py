153a5bf5269deb8b7a24b56cd8c73165db265778
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-06-04 15:39:51 +0200

    modified user serializer create method

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
 from .models import Note
@@ -8,6 +10,17 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "password"]
+    
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user_password = validated_data["password"]
+        try: 
+            validate_password(password=user_password, user=user)
+            user.set_password(validated_data["password"])
+            user.save()
+        except ValidationError as error:
+            raise serializers.ValidationError({"password": error.messages})
+        return user
 
 
 class NoteSerializer(serializers.ModelSerializer):
