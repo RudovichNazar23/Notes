6eb207f4e2e4b387751c7add281fd8b9176386de
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-06-09 21:19:15 +0200

    added inputs for updating note information

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,25 @@
 import NoteDate from "./NoteDate";
 import api from "../utils/api";
+import { useState } from "react";
 
 export default function NoteCard({ note }){
     const { id,  note_title, note_content, note_date_created } = note;
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [inputValue, setInputValue] = useState({
+        "note_title": note_title,
+        "note_content": note_content
+    });
+
+    const onChangeInputValue = (event) => {
+        const inputValue = event.currentTarget.value;
+        const inputId = event.currentTarget.id;
+
+        setInputValue(
+            (prev) => { return {...prev, [inputId]: inputValue} }
+        );
+    };
 
     const onDeleteNote = (event) => {
         event.preventDefault();
@@ -13,17 +30,26 @@ export default function NoteCard({ note }){
     };
 
     return (
-        <div className="card m-1 p-2" style={{width: "500px", height: "250px"}}>
+        <div className="card m-1 p-2" style={{width: "500px", height: "350px"}}>
             <div className="card-body">
-                <h5 className="card-title text-center">{note_title}</h5>
+                {
+                    isOpen ? <input type="text" value={inputValue.note_title} id="note_title" className="form-control" onChange={onChangeInputValue} /> 
+                    : <h5 className="card-title text-center">{note_title}</h5>
+                }
                 <hr />
-                <p className="card-text">{note_content}</p>
+                {
+                    isOpen ? <textarea value={inputValue.note_content} id="note_content" className="form-control" onChange={onChangeInputValue} /> 
+                    : <p className="card-text">{note_content}</p>
+                }
                 <hr />
                 <NoteDate date={note_date_created} />
             </div>
-            <div className="d-flex flex-row justify-content-between" style={{width: "160px"}}>
-                <a className="btn btn-info">Update</a>
-                <a className="btn btn-danger" onClick={onDeleteNote}>Delete</a>
+            <div className="d-flex flex-row justify-content-between" style={{width: "170px"}}>
+                <a className="btn btn-info m-1" onClick={() => setIsOpen(true)}>
+                    Update
+                </a>
+                {!isOpen && <a className="btn btn-danger m-1" onClick={onDeleteNote}>Delete</a>}
+                {isOpen && <a className="btn btn-primary m-1" onClick={() => setIsOpen(false)}>Cancel</a>}
             </div>
         </div>
     );
