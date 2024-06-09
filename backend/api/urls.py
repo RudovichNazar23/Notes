aa66a0e69acbc154e8e70e595b4bdf52e3df217e
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-06-09 21:45:21 +0200

    added view for note updating

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateUserView, MyProfileView, CreateNoteView, NoteListView, DeleteNoteView
+from .views import CreateUserView, MyProfileView, CreateNoteView, NoteListView, DeleteNoteView, UpdateNoteView
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path("create_note/", CreateNoteView.as_view(), name="create_note"),
     path("note_list/", NoteListView.as_view(), name="note_list"),
     path("delete_note/<int:pk>/", DeleteNoteView.as_view(), name="delete_note"),
+    path("update_note/<int:pk>/", UpdateNoteView.as_view(), name="update_note"),
 ]
