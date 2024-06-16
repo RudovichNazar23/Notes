46cfe3d1dfa92c030cf8f9f88237291b1d31fe1c
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-06-16 20:54:04 +0200

    added double click handler for replacing field type at user profile component

--- frontend/src/components/UserProfileComponents/ProfileInfoGroup.jsx
+++ frontend/src/components/UserProfileComponents/ProfileInfoGroup.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 export default function ProfileInfoGroup({ fieldValue }){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const DoubleClickHandler = () => setIsOpen(!isOpen);
+
     return (
         <div className="m-1 d-flex flex-row justify-content-between" style={{width: "65%"}}>
-            <div className="form-control">{fieldValue}</div>
-            <button className="btn btn-primary m-1">Update</button>
+            {
+                isOpen ? (
+                    <>
+                        <input type="text" value={fieldValue} className="form-control" onDoubleClick={DoubleClickHandler}/>
+                        <button className="btn btn-primary m-1">Update</button>
+                    </>
+                ) : (
+                    <div className="form-control" onDoubleClick={DoubleClickHandler}>{fieldValue}</div>
+                )
+            }
         </div>
     );
 };
