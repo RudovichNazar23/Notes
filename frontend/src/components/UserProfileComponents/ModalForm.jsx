9063e1cbbfcf9e9b761637c07fd859ec911f7e4f
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-06-16 21:22:49 +0200

    added new modal form component for changing profile photo

---
+++ frontend/src/components/UserProfileComponents/ModalForm.jsx
@@ -0,0 +1,20 @@
+export default function ModalForm(){
+    return (
+        <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Upload new photo</h5>
+                </div>
+                <div className="modal-body">
+                    <input type="file" className="form-control"></input>
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Save changes</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- frontend/src/components/UserProfileComponents/ProfilePicture.jsx
+++ frontend/src/components/UserProfileComponents/ProfilePicture.jsx
@@ -1,9 +1,17 @@
+import ModalForm from "./ModalForm";
+
 import api from "../../utils/api";
 
 export default function ProfilePicture({ srcPath }){
     return (
-        <div className="p-2" style={{width: "400px", height: "400px"}}>
-            <img src={`${api.defaults.baseURL}/${srcPath}`} width={"100%"} height={"100%"} className="rounded" />
+        <div className="p-2 m-2" style={{width: "400px", height: "400px"}}>
+            <img src={`${api.defaults.baseURL}/${srcPath}`} width={"100%"} height={"100%"} className="rounded m-1" />
+            <div className="p-2 d-flex flex-row align-items-center justify-content-center">
+                <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                    Upload new photo
+                </button>
+            </div>
+            <ModalForm />
         </div>
     );
 };
